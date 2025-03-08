2add2224f3a33eb634d45e31c2ef0d59afdffe52
Himanth3 <163141294+Himanth3@users.noreply.github.com>
2025-03-08 13:15:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,32 +3,48 @@ const char = " " + "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" + "0123456789" + "abcdef
 const charArray = Array.from(char);
 let keyArray = [...charArray];
 
-// Shuffle function
+// Shuffle function (Fisher-Yates algorithm)
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]]; // Swap
     }
 }
+
+// Shuffle keyArray only once
 shuffle(keyArray);
 
 // Encrypt function
 function encrypt(text) {
-    return text.split("").map(letter => keyArray[charArray.indexOf(letter)] || letter).join("");
+    return text.split("").map(letter => {
+        const index = charArray.indexOf(letter);
+        return index !== -1 ? keyArray[index] : letter; // Handle unknown characters
+    }).join("");
 }
 
 // Decrypt function
 function decrypt(text) {
-    return text.split("").map(letter => charArray[keyArray.indexOf(letter)] || letter).join("");
+    return text.split("").map(letter => {
+        const index = keyArray.indexOf(letter);
+        return index !== -1 ? charArray[index] : letter; // Handle unknown characters
+    }).join("");
 }
 
 // UI Functions
 function encryptText() {
     const inputText = document.getElementById("inputText").value;
+    if (!inputText) {
+        alert("Please enter some text to encrypt.");
+        return;
+    }
     document.getElementById("outputText").value = encrypt(inputText);
 }
 
 function decryptText() {
     const inputText = document.getElementById("inputText").value;
+    if (!inputText) {
+        alert("Please enter some text to decrypt.");
+        return;
+    }
     document.getElementById("outputText").value = decrypt(inputText);
 }
